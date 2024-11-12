25338bf9ac9843f904d98707747a09c3994d12a2
MuhammadMagdy7 <melhossiny645@gmail.com>
2024-11-12 06:35:36 +0200

    add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function NotFound() {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-[400px] text-center">
+        <div className="text-red-500 text-6xl mb-4">😕</div>
+        <h2 className="text-2xl font-bold text-gray-800 mb-2">Oops!</h2>
+        <p className="text-gray-600">Page not found</p>
+        <button 
+          onClick={() => window.location.reload()} 
+          className="mt-4 bg-purple-500 text-white px-6 py-2 rounded-full hover:bg-purple-600 transition-colors duration-300"
+        >
+          Try Again
+        </button>
+        
+        <Link href={'/'}className="mt-4 bg-purple-500 text-white px-6 py-2 rounded-full hover:bg-purple-600 transition-colors duration-300"
+        >
+            Back to Home
+        </Link>
+      </div>
+    );
+  }
