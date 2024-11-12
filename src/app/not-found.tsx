6cbad307d47cb624eb8b268a669e0641574e70a2
MuhammadMagdy7 <melhossiny645@gmail.com>
2024-11-12 06:44:14 +0200

    add not found file

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,12 +6,7 @@ export default function NotFound() {
         <div className="text-red-500 text-6xl mb-4">😕</div>
         <h2 className="text-2xl font-bold text-gray-800 mb-2">Oops!</h2>
         <p className="text-gray-600">Page not found</p>
-        <button 
-          onClick={() => window.location.reload()} 
-          className="mt-4 bg-purple-500 text-white px-6 py-2 rounded-full hover:bg-purple-600 transition-colors duration-300"
-        >
-          Try Again
-        </button>
+
         
         <Link href={'/'}className="mt-4 bg-purple-500 text-white px-6 py-2 rounded-full hover:bg-purple-600 transition-colors duration-300"
         >
